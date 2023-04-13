2f1120eded98ac39932e4abfc46180dc0ae78bd6
gxt <15830407570@163.com>
2023-04-13 23:46:00 +0800

    minio method passed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 
 mod minio_client;
 mod sqllit_client;
-
 #[tokio::main]
-pub async fn  main() {
-    minio_client::minio::put_object().await;
-    minio_client::minio::get_object().await;
-   
-    minio_client::minio::delete_object().await;
-    minio_client::minio::get_object().await;
+pub async fn main() {
+    let res =minio_client::minio::put_object("xxx","xxx.pdf").await;
+
+    minio_client::minio::get_object("个人简历.pdf","xxx.pdf").await;
+
+
+
 }
--- src/minio_client/minio.rs
+++ src/minio_client/minio.rs
@@ -1,6 +1,10 @@
 use awsregion::Region;
 use s3::creds::Credentials;
+use s3::error::S3Error;
 use s3::Bucket;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
 pub async fn create_minio() -> Bucket {
     let credentials = Credentials::new(
         Some("TgNBUh5gZ0kj5KeT"),
@@ -24,40 +28,27 @@ pub async fn create_minio() -> Bucket {
     bucket
 }
 
-pub async fn get_object() {
+pub async fn get_object(object_name: &str, local_path: &str) {
     let bucket = create_minio().await;
-    let response = bucket.get_object("/one.text").await.unwrap();
-    println!("{:?}", std::str::from_utf8(response.bytes()));
+    let response = bucket.get_object(object_name).await.unwrap();
+    let mut file = File::create(local_path).unwrap();
+    file.write_all(response.bytes()).unwrap();
 }
-pub async fn put_object() {
+pub async fn put_object(local_path: &str, remote_path: &str) -> Result<u16, S3Error> {
     let bucket = create_minio().await;
-    /* let path = "/home/gxt/Documents/简历.pdf";
-    let file = std::fs::File::open(path).unwrap();
-    let mut bfr = std::io::BufReader::new(file);
-    let mut buf = vec![0; 1024];
-    let d = AsyncRead::poll_read(self, cx, buf);
-    let x =bfr.read(&mut buf);
-    bucket.put_object_stream(&mut x, s3_path).await; */
-    let response = bucket.put_object("/one.text", "123122".as_bytes()).await;
+    let content = fs::read(local_path).unwrap();
+    let response = bucket.put_object(remote_path, &content).await;
     match response {
-        Ok(response) => println!("{:?}", response.status_code()),
-        Err(respnse) => println!("{:?}", respnse),
+        Ok(response) => Ok(response.status_code()),
+        Err(respnse) => Err(respnse),
     }
 }
 
-pub async fn delete_object() {
+pub async fn delete_object(object_name: &str) -> Result<u16, S3Error>{
     let bucket = create_minio().await;
-    let response = bucket.delete_object("/one.text").await;
+    let response = bucket.delete_object(object_name).await;
     match response {
-        Ok(response) => println!("{:?}", response.status_code()),
-        Err(respnse) => println!("{:?}", respnse),
+        Ok(response) => Ok(response.status_code()),
+        Err(respnse) => Err(respnse),
     }
-}
-
-async fn read_file() {
-    /* let path = "/home/gxt/Documents/简历.pdf";
-    let file = std::fs::File::open(path).unwrap();
-    let mut bfr = std::io::BufReader::new(file);
-    let mut buf = vec![0; 1024];
-    bfr.read(buf); */
 }
